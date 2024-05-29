d6fff89d16fd1faa5c973300f487c81c2f4c1964
MelchizedekMunene <melchizedekmunene@gmail.com>
2024-05-29 15:26:47 +0300

    Fixed the storage problem

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //This mathematical algorithm generates the random number 
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-//These are the variables requires in order for the function to work
+//These are the variables required in order for the function to work
 const guesses = document.querySelector(".guesses");
 const lastResult = document.querySelector(".lastResult");
 const lowOrHi = document.querySelector(".lowOrHi");
@@ -10,43 +10,42 @@ const guessSubmit = document.querySelector(".guessSubmit");
 
 let guessCount = 1;
 let resetButtton;
+let previousGuesses = [];
 guessField.focus();
 
 /*Function that checks out the user`s guess with the correct answer and provides output.
 It also checks on the amount of turns left. */
-function checkGuess() {
+function checkGuess () {
     const userGuess = Number(guessField.value);
 
     if(guessCount === 1) {
-        guesses.textContent = "Previous guesses: ";
-      }
-      guesses.textContent = `${"Previous Guesses: "} ${userGuess}`;
-
-    if (userGuess === randomNumber){
-        lastResult.textContent = "Congratulations! You Got It Right!";
-        lastResult.style.backgroundColor = "green";
-        lowOrHi.textContent = " ";
-        setGameOver();
-
-    }else if(guessCount === 10){
-        lastResult.textContent = "Game Over!!";
-        lastResult.style.backgroundColor
-        lowOrHi.textContent = "purple";
-        setGameOver();
-    }else{
-        lastResult.textContent = "Wrong! Try Again!";
-        lastResult.style.backgroundColor = "red";
+      guesses.textContent = "Previous guesses: ";
     }
-    if(userGuess > randomNumber){
-        lowOrHi.textContent = "The number was too high.";
-    }else if(userGuess < randomNumber){
-        lowOrHi.textContent = "The number was too low.";
-    }
-}
+    previousGuesses.push(userGuess);
+    guesses.textContent = `Previous guesses: ${previousGuesses.join(", ")}`;
 
-guessCount++;
-guessField.value = "";
-guessField.focus();
+    if(userGuess === randomNumber) {
+      lastResult.textContent = "Congratulations! You got it right!";
+      lastResult.style.backgroundColor = "green";
+      lowOrHi.textContent = " ";
+      setGameOver();
+    }else if(guessCount === 10) {
+      lastResult.textContent = "Sorry, you ran out of guesses. The number was " + randomNumber;
+      lowOrHi.textContent = " ";
+      setGameOver();
+  }else{
+    lastResult.textContent = "Wrong! Try again!";
+    lastResult.style.backgroundColor = "red";
+    if(userGuess < randomNumber) {
+      lowOrHi.textContent = "Last guess was too low!";
+  }else if(userGuess > randomNumber) {
+    lowOrHi.textContent = "Last guess was too high!";
+  }
+  }
+  guessCount++;
+  guessField.value = " ";
+  guessField.focus();
+  }
 
 guessSubmit.addEventListener("click" , checkGuess);
 
@@ -63,7 +62,8 @@ function setGameOver() {
 //This function that resets everything by clearing the paragraph elements and generating a new random number.
 function resetGame() {
     guessCount = 1;
-
+    previousGuesses = [];
+    
     const resetParas = document.querySelector(".resetParas p");
     for (const resetPara of resetParas) {
         resetPara.textContent = " ";
